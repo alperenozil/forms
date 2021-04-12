58b208584d2f3ee45533192232d134524f8fdabf
alperenozil <necdet.ozil@hacettepe.edu.tr>
2021-04-12 21:41:33 +0300

    Update forms.jsx

--- src/forms.jsx
+++ src/forms.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class Forms extends Component {
     render() { 
-        return ( <div>Login</div> );
+        return ( 
+        <div>
+            <form action="">
+                <div className="form-group">
+                    <label htmlFor=""></label>
+                    <input type="text" className="form-control"/>
+                </div>
+            </form>
+        </div> );
     }
 }
  
